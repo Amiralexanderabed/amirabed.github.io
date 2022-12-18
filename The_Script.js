62b714ed074d19097c19801cf4c64595d5d3d797
Amiralexanderabed <alexanderaa2000@gmail.com>
2022-12-17 22:21:28 -0800

    Updated 2023

--- The_Script.js
+++ The_Script.js
@@ -1,3 +1,26 @@
+var slideIndex = 1;
+showSlides_2(slideIndex);
+
+function currentSlide(n) {
+  showSlides_2(slideIndex = n);
+}
+
+function showSlides_2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active2", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active2";
+}
+
 
 var slideIndex = 0;
 showSlides();
@@ -16,5 +39,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active2";
-  setTimeout(showSlides, 10000); // Change image every 2 seconds
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  
 }
